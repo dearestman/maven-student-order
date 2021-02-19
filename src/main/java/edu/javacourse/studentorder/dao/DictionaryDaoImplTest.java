8a0d7a68d5115a0af667901f24a311b32e029673
Дмитрий Ступаков <dmitrijstupakov@iMac-Dmitrij.local>
2021-02-19 05:44:38 +0300

    Unit tests

---
+++ src/main/java/edu/javacourse/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,12 @@
+package edu.javacourse.studentorder.dao;
+
+
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+    @Test
+    public void testExample(){
+        System.out.println("test");
+    }
+
+}
