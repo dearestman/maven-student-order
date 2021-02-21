98959006b062ce8f4eb1c6132da01c326260abc9
Дмитрий Ступаков <dmitrijstupakov@iMac-Dmitrij.local>
2021-02-21 19:06:12 +0300

    reflection unit tests (junit) less 67

---
+++ src/main/java/edu/javacourse/studentorder/test/DictionaryDaoImplTest.java
@@ -0,0 +1,20 @@
+package edu.javacourse.studentorder.test;
+
+
+import org.junit.jupiter.api.Test;
+
+public class DictionaryDaoImplTest {
+    @Test
+    public void testExample1(){
+        System.out.println("Test1");
+    }
+    @Test
+    public void testExample2(){
+        System.out.println("Test2");
+    }
+    @Test
+    public void testExample3(){
+        System.out.println("Test3");
+        throw new RuntimeException("BAD RESULT!");
+    }
+}
---
+++ src/main/java/edu/javacourse/studentorder/test/SimpleRunner.java
@@ -0,0 +1,38 @@
+package edu.javacourse.studentorder.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class SimpleRunner {
+    public static void main(String[] args) {
+        SimpleRunner sr = new SimpleRunner();
+
+        sr.runTest();
+    }
+
+    private void runTest() {
+        try{
+            Class cl = Class.forName("edu.javacourse.studentorder.test.DictionaryDaoImplTest");
+
+            Constructor cst = cl.getConstructor();
+            Object entity = cst.newInstance();
+
+            Method[] methods = cl.getMethods();
+            for (Method m : methods){
+                Test ann = m.getAnnotation(Test.class);
+                if (ann != null){
+                    m.invoke(entity);
+                }
+
+            }
+
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
+    }
+}
